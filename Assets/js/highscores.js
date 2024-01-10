2d6a4892dc368cedb4471b3a06f59c2dad743fdf
Mark Lewis <mlewis89@gmail.com>
2024-01-10 19:12:50 +1100

    Added basic css formatting

---
+++ Assets/js/highscores.js
@@ -0,0 +1,7 @@
+//read scores from local storage
+
+//render scores from local storage
+
+//clear scores from local storage
+
+//event handler to return to game
---
+++ Assets/js/script.js
@@ -0,0 +1,14 @@
+//html elements
+StartGameEL = document.querySelector("#btn-start");
+ViewScores = document.querySelector("#btn-view-scores");
+QuestionBlockEL = document.querySelector("#quiz-active") ;
+QuestionTiltleEL = document.querySelector("#question-TILT");
+QuestionEL = document.querySelector("#question");
+OptionEL = document.querySelector("#quiz-options");
+
+//event handler to start game on button press
+//event handler to redirect to highscores.html
+//eventhandler to record quiz selection
+
+//list of questions
+//save score to local storage
