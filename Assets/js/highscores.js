c776b9f1fcfc259e50f849ea0eb3a0612ae02408
Mark Lewis <mlewis89@gmail.com>
2024-01-11 10:43:12 +1100

    added event listener for clear score board

--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -1,16 +1,29 @@
 //html Elements
 returnEL = document.querySelector("#return-game");
+ClearScoreBoardEL = document.querySelector("#clear-scores-btn");
 //URLs
 var Page_index = "./index.html";
-
+var storagekey = "quiz-scores";
+var storedScores;
 
 //event handler to return to game
 returnEL.addEventListener('click',function() {
     document.location.href = Page_index;
 })
 
-//read scores from local storage
+ClearScoreBoardEL.addEventListener('click',ClearScoreBoard)
 
-//render scores from local storage
 
 //clear scores from local storage
+function ClearScoreBoard ()
+{
+    localStorage.setItem(storagekey,[]);
+    renderScoreboard();
+}
+
+//render scores from local storage
+function renderScoreboard()
+{
+    //read scores from local storage
+    storedScores = localStorage.getItem(storagekey);
+}
