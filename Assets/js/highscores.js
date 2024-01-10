63a2a6ed9c8e66e085c76bba33efa8b16e5a6bd7
Mark Lewis <mlewis89@gmail.com>
2024-01-10 19:34:38 +1100

    added in event listeners for page change buttons

--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -1,7 +1,16 @@
+//html Elements
+returnEL = document.querySelector("#return-game");
+//URLs
+var Page_index = "./index.html";
+
+
+//event handler to return to game
+returnEL.addEventListener('click',function() {
+    document.location.href = Page_index;
+})
+
 //read scores from local storage
 
 //render scores from local storage
 
 //clear scores from local storage
-
-//event handler to return to game
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,31 @@
 //html elements
 StartGameEL = document.querySelector("#btn-start");
-ViewScores = document.querySelector("#btn-view-scores");
-QuestionBlockEL = document.querySelector("#quiz-active") ;
+ViewScoresEL = document.querySelector("#btn-view-scores");
+QuestionBlockEL = document.querySelector("#quiz-active");
 QuestionTiltleEL = document.querySelector("#question-TILT");
 QuestionEL = document.querySelector("#question");
 OptionEL = document.querySelector("#quiz-options");
+timerEL = document.querySelector("#")
+//URLs
+var Page_scores = "./highscores.html"
 
-//event handler to start game on button press
+
 //event handler to redirect to highscores.html
+ViewScoresEL.addEventListener('click', function () {
+    document.location.href = Page_scores;
+
+});
+//event handler to start game on button press
+StartGameEL.addEventListener('click', function () {
+//start timer
+//for items in questiosn array
+    //render question[i]
+
+});
+
 //eventhandler to record quiz selection
 
 //list of questions
 //save score to local storage
+//set timer
+//clear timer
