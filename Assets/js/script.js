42db8704811f759c6b1a1fc8a0587b1a610fadf4
Mark Lewis <mlewis89@gmail.com>
2024-01-10 21:19:32 +1100

    added in timer function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,12 +2,21 @@
 StartGameEL = document.querySelector("#btn-start");
 ViewScoresEL = document.querySelector("#btn-view-scores");
 QuestionBlockEL = document.querySelector("#quiz-active");
-QuestionTiltleEL = document.querySelector("#question-TILT");
+QuestionTiltleEL = document.querySelector("#question-title");
 QuestionEL = document.querySelector("#question");
 OptionEL = document.querySelector("#quiz-options");
-timerEL = document.querySelector("#")
+timerEL = document.querySelector("#quiz-timer");
+runningscoreEL = document.querySelector("#current-score")
+StartingblockEL = document.querySelector("#start-page");
+QuizBlockEL = document.querySelector("#quiz-block");
+endQuizBlockEL = document.querySelector("#quiz-end-block");
+
 //URLs
 var Page_scores = "./highscores.html"
+
+var timerCount;
+var timerID;
+var currentQuestion = 0;
 
 
 //event handler to redirect to highscores.html
@@ -17,15 +26,59 @@ ViewScoresEL.addEventListener('click', function () {
 });
 //event handler to start game on button press
 StartGameEL.addEventListener('click', function () {
-//start timer
-//for items in questiosn array
+    timerCount = 60;
+    //start timer
+    //loadFirstQuestion();
+    StartingblockEL.setAttribute(class='hidden');
+    QuizBlockEL.setAttribute(class='visible');
+    endQuizBlockEL.setAttribute(class='hidden');
+    StartTimer();
+    nextQuestion();
+    //for items in questiosn array
     //render question[i]
 
 });
 
+
+function nextQuestion()
+{
+
+   
+
 //eventhandler to record quiz selection
 
-//list of questions
+
 //save score to local storage
-//set timer
-//clear timer
+
+//start timer
+function StartTimer(sec) {
+    //
+    timerID = setInterval(function () 
+    {
+        timerCount--;
+        timerEL.textContent = timerCount;
+        if (timerCount <= 0) 
+        {
+            clearInterval(timerID);
+            endGame();
+        }
+     } ,1000);
+}
+
+//list of questions
+questionArr = [
+    {   question: "what is the average airspeed of an unlaiden swallow?", //question
+        options: ["1mph","10kmh","African or europeaN?", "100 kmh"], //list of possible answers
+        answer: 2 //index of the correct answer in the option arr
+    },
+    {   question: "which of the following is primarily responsible for the formatting of a webpage?", //question
+        options: ["css","html","javascript", "gitHub"], //list of possible answers
+        answer: 0 //index of the correct answer in the option arr
+    },
+    {   question: "what does DOM stand for", //question
+        options: ["Document Over Music","Document Object Model","Data Object Mode ", "Display On Mouse"], //list of possible answers
+        answer: 1 //index of the correct answer in the option arr
+    }
+];
+
+
