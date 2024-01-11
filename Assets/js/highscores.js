b16b32a385a07c4109c72d39a2995bc83088ca80
Mark Lewis <mlewis89@gmail.com>
2024-01-11 13:33:19 +1100

    comented code

--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -2,46 +2,53 @@
 returnEL = document.querySelector("#return-game");
 ClearScoreBoardEL = document.querySelector("#clear-scores-btn");
 ScoreboardListEL = document.querySelector("#scoreboard");
-//URLs
+//Global Variables
 var Page_index = "./index.html";
 var storagekey = "quiz-scores";
 
-//event handler to return to game
+//event handler for button click  - to return to game page
 returnEL.addEventListener('click', function () {
     document.location.href = Page_index;
 });
 
+//event handler clear score board on button click
 ClearScoreBoardEL.addEventListener('click', ClearScoreBoard);
 
-//clear scores from local storage
+//clear scores from local storage and update displayed scoreboard
 function ClearScoreBoard() {
     localStorage.setItem(storagekey, JSON.stringify([]));
     renderScoreboard();
 }
 
-//render scores from local storage
+//render scores from local storage to html elements
 function renderScoreboard() {
     //read scores from local storage
     var storedScores = JSON.parse(localStorage.getItem(storagekey));
     ScoreboardListEL.innerHTML = "";
     if (storedScores.length > 0) {
         storedScores[storedScores.length - 1].last = true;
-        var sortedScores = storedScores.sort((a, b) => { if(b.score == a.score){return a.time - b.time}else{return b.score - a.score;} }); //sort Scoreboard by scores in decending order
+        //sort Scoreboard by scores in decending order
+        var sortedScores = storedScores.sort((a, b) => { if(b.score == a.score){return a.time - b.time}else{return b.score - a.score;} });
+        //loop through each score and add it to the scoreboard
         for (var i = 0; i < sortedScores.length; i++) {
+            //create new HTML element
             var li = document.createElement("li");
             li.textContent = sortedScores[i].name + " - " + sortedScores[i].score + " points, in " + sortedScores[i].time + " seconds" ;
-            if(sortedScores[i].last)
-            {
+            //add additional class to the last score in the array
+            if(sortedScores[i].last){
                 li.setAttribute('class','lastAttempt');
             }
+            //add list item to HTML element
             ScoreboardListEL.appendChild(li);
         }
     }
     else {
+        //handle an empty scoreboard condition
         var li = document.createElement("li");
         li.textContent = "Please play the game to recored a score!";
         ScoreboardListEL.appendChild(li);
     }
 }
 
+//reder scoreboard on page load
 renderScoreboard();
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -16,123 +16,130 @@ playerNameEL = document.querySelector("#playername");
 AudioSuccessEL = document.querySelector("#audio-success");
 AudioFailEL = document.querySelector("#audio-fail");
 
-//URLs
+//Global Variables
 var Page_scores = "./highscores.html"
 var storagekey = "quiz-scores";
-
 var timerCount;
 var timerID;
 var timerDefault = 60; //time in seconds of timer
 var currentQuestion = 0;
 var score = 0;
 
-
-//event handler to redirect to highscores.html
+//event handler to redirect to highscores.html on button click
 ViewScoresEL.addEventListener('click', function () {
     document.location.href = Page_scores;
-
 });
+
 //event handler to start game on button press
 StartGameEL.addEventListener('click', function () {
+    //set timer to default value
     timerCount = timerDefault;
-    //start timer
+    //change visiblity of HTML blocks to enable the quiz questione.
     StartingblockEL.setAttribute('class', 'hidden');
     QuizBlockEL.setAttribute('class', 'visible');
-    StartTimer();
+    //load first question
     nextQuestion();
+    //start timer
+    StartTimer();
 });
 
 //eventhandler to record quiz selection
 QuizBlockEL.addEventListener('click', function () {
+    //get user sellection, and convert to integer for comparison with answer
     var buttonID = parseInt(event.target.id.slice(-1));
+    //check if user selected the correct answer
     if (questionArr[parseInt(currentQuestion - 1)].answer == buttonID) {
-        score++;
-        runningscoreEL.textContent = score
-        AudioSuccessEL.play();
+        //if correct
+        score++; // increase score
+        AudioSuccessEL.play(); //play audio effect
     }
     else {
-        timerCount = timerCount - 15;
-        AudioFailEL.play();
+        //if wrong
+        timerCount = timerCount - 15; //decrease remaining time
+        AudioFailEL.play(); //play failed audio effect
     }
-    UpdateScore(score);
-    nextQuestion();
+    UpdateScore(score); //update score elements
+    nextQuestion(); //load the next question
 });
 
 //save score to local storage
 recordScore.addEventListener('submit', function () {
-    event.preventDefault();
-    var storedScores = JSON.parse(localStorage.getItem(storagekey));
-    if (storedScores === null || storedScores === undefined) {
+    event.preventDefault(); //prevent form from clearing input
+    var storedScores = JSON.parse(localStorage.getItem(storagekey)); //retrieve existing data from local storage
+    if (storedScores === null || storedScores === undefined) //if not defined, define as array
+    {
         var storedScores = [];
     }
-    if (playername.value !== "") {
+    if (playername.value !== "") //if user's name is not empy 
+    {
         var playerscore = {
             name: playername.value,
             score: score,
             time: timerDefault - timerCount
-        };
-        storedScores.push(playerscore);
-        localStorage.setItem(storagekey, JSON.stringify(storedScores));
-        document.location.href = Page_scores;
+        }; //store data with an opbject
+        storedScores.push(playerscore); // add object to end of the array
+        localStorage.setItem(storagekey, JSON.stringify(storedScores)); //convert to a string and send to local storage
+        document.location.href = Page_scores;//navigate to high score page
     } else {
-        alert("please enter a name");
+        window.alert("please enter a name");//alert if no name or inials are entered
     }
 })
 
-function UpdateScore(x)
-{
-    score = x;
-    runningscoreEL.textContent = x;
-    finalScoreEL.textContent = x;
-
+function UpdateScore(x) {
+    score = x; //update local viable
+    runningscoreEL.textContent = x; //update score elemet at top of page
+    finalScoreEL.textContent = x; //update score in final section
 }
 
-tryAgainEL.addEventListener('click',function(){
+tryAgainEL.addEventListener('click', function () {
+    //resest visibility to original
     StartingblockEL.setAttribute('class', 'visible');
     QuizBlockEL.setAttribute('class', 'hidden');
     endQuizBlockEL.setAttribute('class', 'hidden');
+    //reset quiz veriables to defauls
     UpdateScore(0);
     currentQuestion = 0;
-
 })
 
 function nextQuestion() {
-    if (currentQuestion >= questionArr.length) //endgame if there are no more questions
+    if (currentQuestion >= questionArr.length) //check if there are more questions
     {
-        endGame();
+        endGame(); //endgame if there are no more questions
         return;
     }
 
-    var questionObj = questionArr[currentQuestion];
-    QuestionTiltleEL.textContent = "Question " + parseInt(currentQuestion + 1) + " of " + questionArr.length;
-    QuestionEL.textContent = questionObj.question;
-    OptionEL.innerHTML = "";
+    var questionObj = questionArr[currentQuestion]; //load question data into temp object
+    QuestionTiltleEL.textContent = "Question " + parseInt(currentQuestion + 1) + " of " + questionArr.length; //display question number and progress
+    QuestionEL.textContent = questionObj.question; //display question
+    OptionEL.innerHTML = ""; //clear data from quiz options 
     for (var i = 0; i < questionObj.options.length; i++) {
-        var li = document.createElement("li");
-        li.textContent = questionObj.options[i];
-        li.setAttribute("id", "quiz-option_" + i);
-        li.setAttribute("class", "quizbutton");
-        OptionEL.appendChild(li);
+        var li = document.createElement("li"); //create list element
+        li.textContent = questionObj.options[i];  //add text of option
+        li.setAttribute("id", "quiz-option_" + i); //add id for use in the answer verification
+        li.setAttribute("class", "quizbutton"); //add class for styling
+        OptionEL.appendChild(li); //add element to the page
     }
     currentQuestion++;
 }
 
 function endGame() {
+    //set block visiblity
     QuizBlockEL.setAttribute('class', 'hidden');
     endQuizBlockEL.setAttribute('class', 'visible');
-    UpdateScore(score);
-    clearInterval(timerID);
+    UpdateScore(score);  //update score elements
+    clearInterval(timerID);//cancel timer
 }
 
 //start timer
 function StartTimer(sec) {
-    //
-    timerID = setInterval(function () {
-        timerCount--;
-        timerEL.textContent = timerCount;
-        if (timerCount <= 0) {
-            clearInterval(timerID);
-            endGame();
+    //create timer element
+    timerID = setInterval(function () { //function to run each time the timer trigers
+        timerCount--; //decrease cound by 1
+        timerEL.textContent = timerCount; //update timer html element
+        if (timerCount <= 0) //when timer runs out
+        {
+            clearInterval(timerID); //cancel timer
+            endGame(); //endgame
         }
     }, 1000);
 }
@@ -142,27 +149,27 @@ questionArr = [
     {
         question: "Which of these is Not a javascrit datatype?", //question
         options: ["Integer", "Boolean", "Css", "String"], //list of possible answers
-        answer:  2//index of the correct answer in the option arr
+        answer: 2//index of the correct answer in the option arr
     },
     {
         question: "Which of the below denotes an array in javascript?", //question
         options: ["''", "[]", "{}", "Arr"], //list of possible answers
-        answer:  1//index of the correct answer in the option arr
+        answer: 1//index of the correct answer in the option arr
     },
     {
         question: "Which of the following is a Method of a javascript string?", //question
         options: [".skin()", ".peel()", ".slice()", ".tape()"], //list of possible answers
-        answer:  2//index of the correct answer in the option arr
+        answer: 2//index of the correct answer in the option arr
     },
     {
         question: "What method would you use to listen for an event?", //question
-        options: ["addEventListener","listen",  "string", "array"], //list of possible answers
-        answer:  0//index of the correct answer in the option arr
+        options: ["addEventListener", "listen", "string", "array"], //list of possible answers
+        answer: 0//index of the correct answer in the option arr
     },
     {
         question: "Which is correct way to read an object called 'name' from local storage?", //question
         options: ["name", "JSON.parse(localStorage.getItem('name'))", "localStorage.value", "localStorage.getitem('name')"], //list of possible answers
-        answer:  1//index of the correct answer in the option arr
+        answer: 1//index of the correct answer in the option arr
     },
     {
         question: "Bonus: What is the average airspeed of an unlaiden swallow?", //question
